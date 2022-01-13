ca0a0b4672652a08eb5eef4c62a02860e61341fa
kramuel <sam.karlstrm@gmail.com>
2022-01-13 16:33:36 +0100

    api call working to frontend

--- frontend/src/PersonalLogin.tsx
+++ frontend/src/PersonalLogin.tsx
@@ -1,8 +1,14 @@
-const PersonalLogin = () => {
+interface Props {
+    loginRoute: string
+}
+
+const PersonalLogin = ({loginRoute}: Props) => {
+    // const backendLoginRoute = 'http://localhost:5005/login'
+    
     return(
         <div className="PersonalLoginBoxCapsule">
         <div className="PersonalLoginBox">
-            <a href="/" className="PersonalLoginLink">
+            <a href={loginRoute} className="PersonalLoginLink">
                 <div className="PersonalLoginTextCapsule"> 
                 <img className="PersonaolLoginSpotifyIcon" src="https://upload.wikimedia.org/wikipedia/commons/7/71/Spotify.png"></img>
                 </div>Get Your Personal History Here 
