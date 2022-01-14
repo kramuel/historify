a05030e8c328b54c705c698308100dac4d9c02e9
erikappelgren <erik.appelgren@hotmail.com>
2022-01-14 17:09:05 +0100

    redesign

--- frontend/src/AboutPage.tsx
+++ frontend/src/AboutPage.tsx
@@ -1,11 +1,18 @@
 const AboutPage = () => {
-    return(
+    return( <>
         <div className="GeneralSpotifyHistoryBox">
         <div>
-            
+        <h1>Erik och Samuels Spotifyapp </h1>
+        Hello and welcome to our web app. We're using Spotify API to get data to represent nicely :)
         </div>
     </div>
-    );
+    <div>
+       <a href="https://github.com/erikappelgren">GITHUB ERIK</a>
+       <br />
+       <a href="https://github.com/kramuel">GITHUB SAMUEL</a>
+       </div>
+       <small>© 2022</small>
+    </>);
 }
 
 export default AboutPage;
---
+++ frontend/src/GetArtists.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import {Artist, TimeTerm, TimePeriod} from './PersonalPage'
+
+interface Props{
+    rangeTerm: TimeTerm
+    timePeriodRange: TimePeriod
+}
+
+const GetArtists = ({rangeTerm, timePeriodRange}: Props) => {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const [query, setQuery] = useState(searchParams.get("access_token"))
+    const [artistList, setArtistList] = useState<Artist[]>([])
+    useEffect(() => {
+        const params = new URLSearchParams()
+        params.append('time_range', rangeTerm)
+        params.append('limit', '25')
+        params.append('offset', '0')
+
+        fetch('https://api.spotify.com/v1/me/top/artists?' + params,
+            {
+                headers: {
+                    'Authorization': 'Bearer ' + query
+                }
+            })
+            .then(res => res.json())
+            .then(data => {
+                let newArtistList: Artist[] = []
+                console.log(data)
+                const topartist = data.items
+                let count = 1
+                topartist.forEach((artist: any) => {
+                    let newArtist: Artist = {
+                        name: artist.name,
+                        image: artist.images[1].url,
+                        index: count,
+                        imageSize: 320
+                    }
+                    newArtistList.push(newArtist)
+                    count++
+                });
+
+                setArtistList(newArtistList)
+            })
+            .catch(err => console.error(err))
+    }, [rangeTerm]
+    )
+    return(
+    <div className="PersonalGraphCapsule">
+                <div className="GraphList">
+                    <h3>Your Top Artists {timePeriodRange}</h3>
+                    
+                    <div className="imageBoxCapsule">
+                    {artistList.map((artist, index) => {
+                        return (
+                            <div key={index} className="imageDivBox"> <p>{index + 1}. {artist.name}</p><img className='imageDiv' src={artist.image} /></div>
+                        )
+                    })}
+                </div>
+            </div>
+     </div>
+     )
+}
+
+export default GetArtists;
